f8bc1a36ad4b6d93bedcba70ade10363c2aa6afa
Michal Žamboch <zamboch.michal@seznam.cz>
2023-01-16 20:41:07 +0100

    export script update

--- scripts/export.ps1
+++ scripts/export.ps1
@@ -1,6 +1,17 @@
 # Source tutorial: https://www.youtube.com/watch?v=h7II1fiaWKA
 
-$zipFile = ".\game.zip"
+$zipFile = "game.zip"
+$loveFile = "game.love"
+
+# ------------------------------------------------------------------
+
+if (Test-Path $zipFile) {
+    Remove-Item $zipFile
+}
+
+if (Test-Path $loveFile) {
+    Remove-Item $loveFile
+}
 
 Compress-Archive -Path ..\src\* -DestinationPath $zipFile
-Rename-Item -Path $zipFile -NewName "game.love"
+Rename-Item -Path $zipFile -NewName $loveFile
