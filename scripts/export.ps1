4c088527901f28281d140787304e34407b804838
Michal Žamboch <zamboch.michal@seznam.cz>
2023-01-18 17:45:28 +0100

    script update

--- scripts/export.ps1
+++ scripts/export.ps1
@@ -5,10 +5,11 @@ $loveFile = "game.love"
 $url = "https://github.com/love2d/love/releases/download/11.4/love-11.4-win64.zip"
 $exportFolder = "love-11.4-win64"
 $tmpZipFile = "love-win64.zip"
-$destination = "."
 
 $loveExecutable = $exportFolder + "\love.exe"
 $exportLoveFile = $exportFolder + "\" + $loveFile
+
+$finalExportFolder = "Pong"
 
 # ------------------------------------------------------------------
 
@@ -24,6 +25,10 @@ function Cleanup() {
     if (Test-Path $exportFolder) {
         Remove-Item $exportFolder -Force -Recurse
     }
+    
+    if (Test-Path $finalExportFolder) {
+        Remove-Item $finalExportFolder -Force -Recurse
+    }
 }
 
 function MakeLoveFile() {
@@ -33,7 +38,7 @@ function MakeLoveFile() {
 
 function MakeTargetDirectory() {
     Invoke-WebRequest -URI $url -OutFile $tmpZipFile
-    Expand-Archive -Path $tmpZipFile -DestinationPath $destination -Force
+    Expand-Archive -Path $tmpZipFile -DestinationPath "." -Force
     Remove-Item $tmpZipFile
 }
 
@@ -41,11 +46,23 @@ function MoveLoveFile() {
     Move-Item -Path $loveFile -Destination $exportFolder
 }
 
+function PrepareExportFolder() {
+    New-Item -Path "." -Name $finalExportFolder -ItemType "directory"
+    Get-ChildItem -Path $exportFolder -Include *.dll -Recurse | Copy-Item -Destination $finalExportFolder
+}
+
 function MakeExeFile() {
-    $cmdToRun = "copy /b " + $loveExecutable + " + " + $exportLoveFile + " game.exe"
+    $cmdToRun = "copy /b " + $loveExecutable + " + " + $exportLoveFile + " " + $finalExportFolder + "\game.exe"
+    Set-Clipboard -Value $cmdToRun
+    
+    Write-Host
     Write-Host "After opening Command Line run this command:" -ForegroundColor Cyan
     Write-Host $cmdToRun -ForegroundColor Cyan
+    Write-Host 
+    Write-Host "Command is copied in your clipboard." -ForegroundColor Cyan
+    Write-Host "Paste it with right click." -ForegroundColor Cyan
     Write-Host
+
     cmd.exe
 }
 
@@ -55,4 +72,5 @@ Cleanup
 MakeLoveFile
 MakeTargetDirectory
 MoveLoveFile
+PrepareExportFolder
 MakeExeFile
