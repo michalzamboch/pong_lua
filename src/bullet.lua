f249d94622930a05c99b4bdb035e988864b6142f
Michal Žamboch <zamboch.michal@seznam.cz>
2023-01-28 18:58:30 +0100

    bullet preparations

--- src/bullet.lua
+++ src/bullet.lua
@@ -1,39 +1,107 @@
 require "configuration"
 
 Bullet = {
+    game = {},
     fired = false,
     x = 0,
     y = 0,
-    r = BulletRadius,
+    a = BulletRadius,
     speed = BulletSpeed
 }
+
+--[[
+fired
+x
+y
+a
+speed
+]]
+
+-------------------------------------------------------
 
 local function DefaultSettings(o)
     o.x = 0
     o.y = 0
-    o.r = BulletRadius
+    o.a = BulletRadius
     o.speed = BulletSpeed
     o.fired = false
 end
 
-function Bullet:new(x, y)
+function Bullet:new(game, x, y)
     local object = {}
     setmetatable(object, self)
     self.__index = self
 
     DefaultSettings(object)
+
+    self.game = game
+    self.x = x
+    self.y = y
+
     return object
 end
 
-function Bullet:draw()
-    love.graphics.circle("fill", self.x, self.y, self.r)
-end
-
-function Bullet:move()
-    local dt = love.timer.getDelta()
-    local tmp_x = self.x + self.speed * dt * MotionConstant
-end
+-------------------------------------------------------
 
 function Bullet:reset()
     DefaultSettings(self)
 end
+
+function Bullet:draw()
+    love.graphics.circle("fill", self.x, self.y, self.a)
+end
+
+function Bullet:move()
+    local dt = love.timer.getDelta()
+    local tmp_x = self.x
+    self.x = self.x + self.speed * dt * MotionConstant
+
+    --self.x = tmp_x
+end
+
+-------------------------------------------------------
+
+function Bullet:getX1()
+    return self.x - self.a / 2
+end
+
+function Bullet:getY1()
+    return self.y - self.a / 2
+end
+
+function Bullet:getX2()
+    return self.x + self.a / 2
+end
+
+function Bullet:getY2()
+    return self.y + self.a / 2
+end
+
+-------------------------------------------------------
+
+function Bullet:copy(object)
+    self.fired = object.fired
+    self.x = object.x
+    self.y = object.y
+    self.a = object.a
+    self.speed = object.speed
+end
+
+function Bullet:toString()
+    return tostring(self.fired) .. " " ..
+        tostring(self.x) .. " " ..
+        tostring(self.y) .. " " ..
+        tostring(self.a) .. " " ..
+        tostring(self.speed)
+end
+
+function Bullet:fromString(string)
+    local data = Split(string, " ")
+    self.fired = toboolean(data[1])
+    self.x = tonumber(data[2])
+    self.y = tonumber(data[3])
+    self.a = tonumber(data[4])
+    self.speed = tonumber(data[5])
+end
+
+-------------------------------------------------------
--- src/physics.lua
+++ src/physics.lua
@@ -1,3 +1,7 @@
+function DistanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+end
+
 function PointIn(x, y, r)
     return ((x >= r:getX() and y >= r:getY()) and
         (x <= r:getX2() and y >= r:getY()) and
@@ -12,12 +16,19 @@ function RectInRect(r1, r2)
         PointIn(r1:getX2(), r1:getY2(), r2))
 end
 
+function CircleInRect(circle, rect)
+    return (PointIn(circle:getX1(), circle:getY1(), rect) or
+        PointIn(circle:getX2(), circle:getY1(), rect) or
+        PointIn(circle:getX1(), circle:getY2(), rect) or
+        PointIn(circle:getX2(), circle:getY2(), rect))
+end
+
 function CollidesRect(r1, r2)
     return RectInRect(r1, r2) or RectInRect(r2, r1)
 end
 
 function SpeedUpBounce(ballSpeed, batDirection)
-    if ballSpeed ~= 0  then
+    if ballSpeed ~= 0 then
         return batDirection
     else
         return 0
